e3feeb79deca68040a9b25735ebd9c841a9ba8ef
Jonathan <JohnAnBer@gmail.com>
2026-01-04 15:25:07 -0500

    idk

--- src/1-object-operations.js
+++ src/1-object-operations.js
@@ -1,1 +1,11 @@
-// Create your object here
+const Cart = {
+    customerName : 'Alex',
+    totalPrice : 0,
+    itemCount : 0
+};
+
+Cart.items = ['apples', 'bananas', 'cherries']
+Cart.itemCount = 3;
+Cart.totalPrice = 99.99;
+delete Cart.customerName;
+console.log(Cart)
